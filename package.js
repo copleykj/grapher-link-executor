e202864282fa981eabd814e10a756206cd5bea32
Kelly Copley <copleykj@gmail.com>
2020-12-27 16:12:28 -0500

    update versionsFrom

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'copleykj:grapher-link-executor',
-  version: '1.0.0',
+  version: '1.0.1',
   summary: 'Allow defining grapher links alongside their collections',
   git: 'https://github.com/copleykj/grapher-link-executor.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.8.3');
+  api.versionsFrom('1.12');
   api.use(['typescript', 'mongo', 'cultofcoders:grapher@1.3.19']);
   api.use();
   api.mainModule('grapher-link-executor.ts');
